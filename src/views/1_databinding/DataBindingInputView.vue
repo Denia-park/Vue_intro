9d609a373c12f5b1bc707cac5aaa3260440d2951
Hyunggi <phg9587@naver.com>
2022-07-20 00:58:50 +0900

    Add : DataBindingInputView.vue 파일 추가
    데이터바인딩 관련 내용 정리 - Input [양방향 데이터 바인딩]

---
+++ src/views/1_databinding/DataBindingInputView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!--양방향 데이터 바인딩-->
+    <input type="text" v-model="userId">
+    <button @click="myFunction">클릭</button>
+    <button @click="changeMyData">변경</button>
+    <br>
+    <!--  할당되는 값들은 문자열로 처리됨  -->
+    <input type="text" v-model="num1"> +
+    <input type="text" v-model="num2"> =
+    <span>{{num1 + num2}}</span>
+    <br>
+    <!--  .number를 붙여줬기 때문에 할당되는 값들은 숫자로 처리됨  -->
+    <input type="text" v-model.number="num3"> +
+    <input type="text" v-model.number="num4"> =
+    <span>{{num3 + num4}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+      userId: 'Hyunggi',
+      num1: 0,
+      num2: 0,
+      num3: 0,
+      num4: 0
+
+    }
+  },
+  setup () {
+  },
+  created () {
+  },
+  mounted () {
+  },
+  unmounted () {
+  },
+  methods: {
+    myFunction () {
+      console.log(this.userId)
+    },
+    changeMyData () {
+      this.userId = 'assaads'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
