05da5bd66440f3fbefed2ea302c7e6abb681dd92
Hyunggi <phg9587@naver.com>
2022-07-19 23:34:42 +0900

    Add : DataBindingStringView.vue 파일 추가
    데이터바인딩 관련 내용 정리 - String

---
+++ src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Hello {{username}}</h1>
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: 'John Doe',
+      message: 'Welcome 개발자의 품격'
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
