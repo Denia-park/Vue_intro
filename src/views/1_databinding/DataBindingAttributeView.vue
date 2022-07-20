62492393181c4709d265c04b1a48690c93ed64f0
Hyunggi <phg9587@naver.com>
2022-07-21 00:28:17 +0900

    Add : DataBindingAttributeView.vue 파일 추가
    데이터바인딩 관련 내용 정리 - Attribute [ Tag의 속성값을 바인딩 하는 법]

---
+++ src/views/1_databinding/DataBindingAttributeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <!-- v-bind:value 랑 :value 는 동일한 속성값 -->
+    <input type="text" name="" id="" v-bind:value="userId" readonly>
+    <input type="text" name="" id="" :value="userId" readonly>
+    <br>
+    <img :src="imgSrc" alt="" style="width:100px; height:auto;">
+    <br>
+    <input type="search" name="" id="" v-model="txt1">
+    <button :disabled="txt1 === ''">조회</button>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+      userId: 'Hyunggi',
+      imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png',
+      txt1: ''
+
+    }
+  },
+  setup () {
+  },
+  created () {
+  },
+  mounted () {
+  },
+  unmounted () {
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
