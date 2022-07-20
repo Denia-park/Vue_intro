efbf19ff2f38480c72b51c5a9276537b0a5327d5
Hyunggi <phg9587@naver.com>
2022-07-21 00:49:06 +0900

    Add : DataBindingListView.vue 파일 추가
    데이터바인딩 관련 내용 정리 - List [ 배열의 값을 쉽게 적용하는 법 ]

--- src/router/index.js
+++ src/router/index.js
@@ -49,7 +49,13 @@ const routes = [
     path: '/databinding/attr',
     name: 'DataBindingAttributeView',
     component: () => import(/* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingAttributeView.vue')
+  },
+  {
+    path: '/databinding/list',
+    name: 'DataBindingListView',
+    component: () => import(/* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingListView.vue')
   }
+
 ]
 
 const router = createRouter({
