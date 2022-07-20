9a79155001e3e1aad7a76d5b6f8f7add6d28688c
Hyunggi <phg9587@naver.com>
2022-07-21 00:15:58 +0900

    Add : DataBindingRadiotView.vue 파일 추가
    데이터바인딩 관련 내용 정리 - Radio [양방향 데이터 바인딩]

---
+++ src/views/1_databinding/DataBindingRadioView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div>
+      <input type="radio" id="html" value="HTML" v-model="favoriteLang">
+      <label for="html">HTML</label>
+    </div>
+    <div>
+      <input type="radio" id="css" value="CSS" v-model="favoriteLang">
+      <label for="css">CSS</label>
+    </div>
+    <div>
+      <input type="radio" id="js" value="JS" v-model="favoriteLang">
+      <label for="js">JavaScript</label>
+    </div>
+    <!--위의 체크박스에서 체크할때마다 아래에 선택된 값이 뜸 왜냐? v-model로 묶었기 때문에-->
+    <!--checkbox는 사용자가 변경할 수 있는게 checked 속성밖에 없기-->
+    <!--때문에 checked 속성이 변경되면 value 값을 가지고 와서 처리한다.-->
+    <div>
+      선택한 지역 : {{favoriteLang}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+      // radio는 여러개를 선택할 수 없기 때문에
+      // String 으로 data를 정의하고 바인딩 해야한다. ★반드시 String★
+      // 'JS' 값이 들어가있으면 사이트가 열릴때 JavaScript가 체크된 상태로 열림
+      favoriteLang: ''
+
+    }
+  },
+  setup () {
+  },
+  created () {
+  },
+  mounted () {
+  },
+  unmounted () {
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
